ffc3a5ffd62cbd077286700d2bd58a230119803b
SnazzyPanda <35570455+SnazzyPanda@users.noreply.github.com>
2026-01-17 18:21:06 -0600

    Replace reference to a local archive I used, improve flatpak build script
    Also fixed a bad reference to the icon in the .desktop

--- build-flatpak.sh
+++ build-flatpak.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+REPO_DIR="$PWD/build/flatpak-repo"
 
-flatpak-builder --repo=build-flatpak-repo build/flatpak com.github.snazzypanda.limo.json --force-clean
-flatpak build-bundle build-flatpak-repo build/com.github.snazzypanda.limo.flatpak com.github.snazzypanda.limo
+if [ ! -d "$REPO_DIR" ]; then
+	mkdir "$REPO_DIR"
+fi
+
+flatpak-builder --repo="$REPO_DIR" build/flatpak com.github.snazzypanda.limo.json --force-clean --require-changes --disable-tests --disable-updates
+flatpak build-bundle "$REPO_DIR" build/com.github.snazzypanda.limo.flatpak com.github.snazzypanda.limo
+
